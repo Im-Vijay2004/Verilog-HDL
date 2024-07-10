9d175fa10195a52a4ce12a4077528fde419d980a
Im-Vijay2004 <135182896+Im-Vijay2004@users.noreply.github.com>
2024-07-10 15:29:20 +0530

    Update FA.v

--- FA.v
+++ FA.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 17.01.2024 14:36:10
-// Design Name: 
-// Module Name: FA
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module FA(a,b,c,cy,sum);
 input a,b,c;
 output cy,sum;
